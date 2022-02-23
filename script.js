34f8f2482652bb20a053b78af3c727e1116f8e46
albertofanini-prog <albertofanini@outlook.com>
2022-02-23 16:51:27 +0100

    2nd Commit - Messaggio a schermo

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 console.log('Hello Vue JS!');
+
+//Stampare a schermo un messaggio all'interno di un h1 utilizzando i data
+//Aggiungere alla pagina un'immagine, presa anch'essa da un data
+
+const main = new Vue({
+    el: '#main',
+    data: {
+        input:'Scrivi qui il tuo messaggio',
+        message:'',
+    },
+});
+
+console.log(main);
